eb742a3038304d035d383f5b0d88290c8bb6ee96
Juan Garcia <jugarciale@unal.edu.co>
2025-06-17 04:21:44 -0500

    Adicion de codigo separado para los modulos de RX y TX

---
+++ TX/AgroLoRaProjectTX.ino
@@ -0,0 +1,28 @@
+#include "config.h"
+#include "sensors/bme280_sensor.h"
+#include "moisture/sen0193.h"
+#include "display/oled_display.h"
+#include "lora/lora_comm.h"
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  initBME280();
+  initSoilSensor();
+  initOLED();
+  initLoRa();
+}
+
+void loop() {
+  float t = getTemperature();
+  float h = getHumidity();
+  float p = getPressure();
+  int m = readSoilMoisture();
+
+  String packet = String(t) + "," + String(h) + "," + String(p) + "," + String(m);
+  sendLoRaPacket(packet);
+
+  displayData(t, h, p, m);
+
+  delay(5000);
+}
