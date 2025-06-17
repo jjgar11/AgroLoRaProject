26fb1ad7ad71eb3c648f750d74486bd45614ed23
Juan Garcia <jugarciale@unal.edu.co>
2025-06-17 04:23:03 -0500

    Correccion de visualizacion de datos en RX

--- RX/AgroLoRaProjectRX.ino
+++ RX/AgroLoRaProjectRX.ino
@@ -1,27 +1,32 @@
 #include "config.h"
-#include "sensors/bme280_sensor.h"
-#include "moisture/sen0193.h"
 #include "display/oled_display.h"
 #include "lora/lora_comm.h"
 
 void setup() {
   Serial.begin(115200);
   Wire.begin();
-  initBME280();
-  initSoilSensor();
   initOLED();
   initLoRa();
 }
 
 void loop() {
-  float t = getTemperature();
-  float h = getHumidity();
-  float p = getPressure();
-  int m = readSoilMoisture();
-
   String packet = receiveLoRaPacket();
 
-  displayData(packet);
+  float t = 0, h = 0, p = 0;
+  int m = 0;
+
+  int i1 = packet.indexOf(',');
+  int i2 = packet.indexOf(',', i1 + 1);
+  int i3 = packet.indexOf(',', i2 + 1);
+
+  if (i1 != -1 && i2 != -1 && i3 != -1) {
+    t = packet.substring(0, i1).toFloat();
+    h = packet.substring(i1 + 1, i2).toFloat();
+    p = packet.substring(i2 + 1, i3).toFloat();
+    m = packet.substring(i3 + 1).toInt();
+
+    displayData(t, h, p, m);
+  }
 
   delay(5000);
 }
